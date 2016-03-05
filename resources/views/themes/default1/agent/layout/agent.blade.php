a01ff22572fb7dd169b3e1789bca5262cb1537fc
Serdar SANRI <ssanri@aol.com>
2016-03-05 01:57:30 -0500

    filebrowser removed

--- resources/views/themes/default1/agent/layout/agent.blade.php
+++ resources/views/themes/default1/agent/layout/agent.blade.php
@@ -385,7 +385,7 @@ $(function() {
                     <!-- CK Editor -->
                     <!-- // <script src="{{asset("//cdn.ckeditor.com/4.4.3/standard/ckeditor.js")}}"></script> -->
                     <script src="{{asset("ckeditor/ckeditor.js")}}"></script>
-                    <script src="{{asset("lb-faveo/plugins/filebrowser/plugin.js")}}"></script>
+                    <!--<script src="{{asset("lb-faveo/plugins/filebrowser/plugin.js")}}"></script>-->
                     <script src="{{asset("lb-faveo/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js")}}" type="text/javascript"></script>
                     <script>
                         // $(function () {
