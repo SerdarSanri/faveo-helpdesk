9f1d407de4f5b07440b90fcd35ce3de7ae557408
Serdar SANRI <ssanri@aol.com>
2016-03-05 02:08:47 -0500

    stars

--- resources/views/themes/default1/agent/helpdesk/ticket/timeline.blade.php
+++ resources/views/themes/default1/agent/helpdesk/ticket/timeline.blade.php
@@ -1991,8 +1991,8 @@ function lockAjaxCall(locktime){
         })
 }
 
-
-        $.fn.stars = function() {
+	
+        $.fn['stars'] = function() {
             return $(this).each(function() {
                 $(this).html($('<span />').width(Math.max(0, (Math.min(5, parseFloat($(this).html())))) * 16));
             });
