2e6654260733ed215e4100aebd376960efbd38e3
Serdar SANRI <ssanri@aol.com>
2016-03-05 01:56:45 -0500

    ckeditor bug

--- resources/views/themes/default1/agent/layout/agent.blade.php
+++ resources/views/themes/default1/agent/layout/agent.blade.php
@@ -384,7 +384,7 @@ $(function() {
                      <!-- // <script src="{{asset("dist/js/editor.js")}}"></script> -->
                     <!-- CK Editor -->
                     <!-- // <script src="{{asset("//cdn.ckeditor.com/4.4.3/standard/ckeditor.js")}}"></script> -->
-                    {{-- // <script src="{{asset("ckeditor/ckeditor.js")}}"></script> --}}
+                    <script src="{{asset("ckeditor/ckeditor.js")}}"></script>
                     <script src="{{asset("lb-faveo/plugins/filebrowser/plugin.js")}}"></script>
                     <script src="{{asset("lb-faveo/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js")}}" type="text/javascript"></script>
                     <script>
